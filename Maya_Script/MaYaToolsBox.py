ad77a9999e875572a42507d489599b1b70291025
BlackC.Yang <BlackC.Yang@hotmail.com>
2021-11-04 14:45:10 +0800

    CtrlTool 更新1.0.1

--- Maya_Script/MaYaToolsBox.py
+++ Maya_Script/MaYaToolsBox.py
@@ -1,50 +1,100 @@
 # -*- coding: UTF-8 -*-
-'''Roadmap:1.带UI的直接生成在主窗口中，共用一个button.
+'''Roadmap:
 '''
 from maya import cmds, mel
+from maya.api import OpenMaya as om
+import sys
+import os
+#try:
+#    from importlib import reload
+#except :
+
+sys.path.append('%s/MyToolBox' %os.getenv('ALLUSERSPROFILE'))
+from CopyWeightTool import *
+from CtrlTool import *
+from cur2IK_FX import *
+from DataSaveUi import *
+from PSDshape import *
+from Rivet import *
+from WeightTool import *
 
 
-class MaYaToolsBox():
+class MayaToolsBox_BlackC():
 
-    __Verision = 1.0
+    #__Verision = 1.1
     
-    def __init__(self):
-        self.Info = {
-        'createloc': [u'在选择物体的位置创建Locator',],
-        'polytoCurve': [u'批量提取曲线__仅适用于单片模型',],
-        'movevtx_UI': [u'修型时传递点 \n选择要传递的点 填写被传递的模型',],
-        'samevtx_UI': [u'移动点达到对称修形 \n选择原模型上要对称的点 分别填写模型',],
-        'xiuxingJoint': [u'创建修型骨骼(高自定义) \n选择要修型的骨骼',],
-        'xiuxingJointWang': [u'创建修型骨骼(乖孙版) \n选择要修型的骨骼',],
-        'TransferUV': [u'传递UV \n选择UV模型+要传递的模型',],
-        'createFollicleOnface_UI': [u'在平面上创建毛囊和骨骼',],
-            }
-
     def ToolUi(self):
+        Info = [
+            [u'创建Locator', u'在选择物体的位置创建Locator', 'self.createLocator()'],
+            [u'从模型提取曲线', u'批量提取曲线 - 仅适用于单片模型', 'self.polytoCurve()'],
+            [u'movevtx_Ui', u'修型时传递点 \n选择要传递的点 填写被传递的模型', 'self.movevtx_UI()'],
+            [u'samevtx_Ui', u'移动点达到对称修形 \n选择原模型上要对称的点 分别填写模型', 'self.samevtx_UI()'],
+            [u'修型骨骼xu', u'创建修型骨骼(高自定义) \n选择要修型的骨骼', 'self.xiuxingJoint()'],
+            [u'修型骨骼Hang', u'创建修型骨骼(航少版) \n选择要修型的骨骼', 'self.xiuxingJointHang()'],
+            [u'传递UV', u'传递UV \n选择UV模型+要传递的模型', 'self.TransferUV()'],
+            [u'曲面上创建毛囊和骨骼_Ui', u'在surface曲面上创建毛囊和骨骼', 'self.createFollicleOnsurface_UI()'],
+            [u'ngRelax', u'ngRelax权重', 'self.doPlugin("ngRelax")'],
+            [u'拷贝权重工具_Ui', u'拷贝权重工具', 'CopyWeightTool().Ui()'],
+            [u'动力学曲线 IK_Ui', u'动力学曲线 IK', 'cur2IK_FX_Ui()'],
+            [u'临时储存物体或位置_Ui', u'临时储存物体或位置', 'DataSaveUi().Ui()'],
+            [u'PSD修型_Ui', u'PSD修型', 'PSD_PoseUi_KitKat().ToolUi()'],
+            [u'Rivet', u'Rivet铆钉', 'cRivet("follicle")'],
+            [u'权重工具_Ui', u'点权重调整 \nSave/Load权重', 'WeightTool_JellyBean().ToolUi()'],
+            [u'权重检查工具_Ui', u'权重最大影响值检查 清理', 'WeightCheckTool_JellyBean().ToolUi()'],
+            [u'控制器Pro_Ui', u'权哥 控制器生成', 'My_CtrllTool().Ui()']
+        ]
+        self.Info = sorted(Info, key=lambda item: item[0])
+
         ToolUi = 'MaYaToolsBox'
         if cmds.window(ToolUi, q=1, ex=1):
             cmds.deleteUI(ToolUi)
         cmds.window(ToolUi, t=ToolUi, rtf=1, mb=1, mxb=0, wh=(230, 500))
         cmds.columnLayout('MainCL', cat=('both', 2), rs=2, cw=220, adj=1)
         cmds.textField('searchText', h=24, tcc=lambda *args: self.refreshToolList(cmds.textField('searchText', q=1, tx=1)))
-        cmds.textScrollList('ToolList', ams=0, h=200, sc=lambda *args:
-                                cmds.text('detailText', e=1, l=self.Info[cmds.textScrollList('ToolList', q=1, si=1)[0]]))
+        cmds.textScrollList('ToolList', ams=0, h=250, sc=lambda *args:self.changeToolInfo())
         cmds.columnLayout('EditCL', cat=('both', 2), rs=2, cw=220, adj=1)
         cmds.setParent('..')
-        cmds.text('detailText', p='EditCL', h=100, l='说明:')
-        cmds.button(l='执行', c=lambda *args: eval('MaYaToolsBox().%s()' % (cmds.textScrollList('ToolList', q=1, si=1)[0])))
-
+        cmds.text('detailText', p='EditCL', h=100, l=u'说明:')
+        cmds.button(l=u'执行', c=lambda *args: self.doProc())
         for i in self.Info:
-            cmds.textScrollList('ToolList', e=1, a=i)
+            cmds.textScrollList('ToolList', e=1, a=i[0])
         cmds.showWindow(ToolUi)
+
+    def changeToolInfo(self):
+        slitem = cmds.textScrollList('ToolList', q=1, si=1)[0]
+        for i in self.Info:
+            if slitem == i[0]:
+                cmds.text('detailText', e=1, l=i[1])
 
     def refreshToolList(self, string):
         cmds.textScrollList('ToolList', e=1, ra=1)
+        Istr = string.lower()
         for i in self.Info:
-            if string.lower() in i.lower() or string.lower() in self.Info[i][0].lower():
-                cmds.textScrollList('ToolList', e=1, a=i)
+            if Istr in i[0].lower() or Istr in i[1].lower():
+                cmds.textScrollList('ToolList', e=1, a=i[0])
 
-    def chuangjian(self):
+    def doProc(self):
+        slitem = cmds.textScrollList('ToolList', q=1, si=1)[0]
+        for i in self.Info:
+            if slitem == i[0]:
+                cmd = i[2]
+                exec(cmd)
+
+    def doPlugin(self, File):
+        if File == 'ngRelax':
+            ver = int(cmds.about(v=1))
+            if ver<2017 or ver>2020:
+                om.MGlobal.displayError(u'仅支持2017-2020')
+                return
+            if cmds.pluginInfo('ngSkinTools.mll', q=1, l=1):
+                cmds.ngSkinRelax()
+                return
+            plugName = '%s%s.mll' %(File, ver)
+            if not cmds.pluginInfo(plugName, q=1, l=1):
+                cmds.loadPlugin('%s/MyToolBox/plugin/%s' %(os.getenv('ALLUSERSPROFILE'), plugName), qt=1)
+            cmds.ngSkinRelax()
+
+    def createLocator(self):
         alist = cmds.ls(sl=1, fl=1)
         for i in alist:
             txyz = cmds.xform(i, q=1, ws=1, t=1)
@@ -79,7 +129,7 @@ class MaYaToolsBox():
             pass
         cmds.window(ui, t='movevtx')
         cmds.columnLayout(rowSpacing=3)
-        cmds.textFieldGrp('UI1objTextFieldGrp', l='模型', h=28, cw2=(30, 130))
+        cmds.textFieldGrp('UI1objTextFieldGrp', l=u'模型', h=28, cw2=(30, 130))
         cmds.button('UI1RunButton', l="Run", h=28, w=100, c=lambda*args: self.movevtx(cmds.textFieldGrp('UI1objTextFieldGrp', q=1, tx=1)))
         cmds.window(ui, e=True, wh=(180, 100))
         cmds.showWindow(ui)
@@ -95,14 +145,6 @@ class MaYaToolsBox():
             cmds.select(targe, r=1)
             cmds.move(flo[0], flo[1], flo[2], r=1, os=1, wd=1)
         cmds.select(cl=1)
-        for i in range(8):
-            cmds.sphere()
-        '''
-        cmds.progressWindow(isInterruptable=1)
-        while 1:
-            if cmds.progressWindow(q=1,isCancelled=1): break
-        cmds.progressWindow(endProgress=1)
-        '''
 
     def samevtx_UI(self):
         # UI
@@ -113,8 +155,8 @@ class MaYaToolsBox():
             pass
         cmds.window(ui, t='samevtx')
         cmds.columnLayout(rowSpacing=3)
-        cmds.textFieldGrp('UI2obj1TextFieldGrp',l='已修形模型', h=28, cw2=(60, 150))
-        cmds.textFieldGrp('UI2obj2TextFieldGrp',l='要对称模型', h=28, cw2=(60, 150))
+        cmds.textFieldGrp('UI2obj1TextFieldGrp',l=u'已修形模型', h=28, cw2=(60, 150))
+        cmds.textFieldGrp('UI2obj2TextFieldGrp',l=u'要对称模型', h=28, cw2=(60, 150))
         cmds.button('RunButton', l="Run", h=28, w=100, c=lambda *args:
                     self.samevtx(cmds.textFieldGrp('UI2obj1TextFieldGrp', q=1, tx=1), cmds.textFieldGrp('UI2obj2TextFieldGrp', q=1, tx=1)))
         cmds.window(ui, e=True, wh=(220, 100))
@@ -171,7 +213,7 @@ class MaYaToolsBox():
         cmds.connectAttr(floatMathC + ".outFloat", blendJointEnd + ".t" + Taxial, f=1)
         cmds.connectAttr(joint + ".rotate" + Raxial, floatMathB + ".floatA", f=1)
 
-    def xiuxingJointWang(self):
+    def xiuxingJointHang(self):
         jot_name = cmds.ls(sl=1, typ="joint")
         jot_bs_name1 = cmds.joint(n=(jot_name[0] + "_bs"), rad=3)
         jot_bs_name2 = cmds.joint(n=(jot_name[0] + "_bsend"), rad=3)
@@ -233,7 +275,7 @@ class MaYaToolsBox():
         else:
             cmds.polyTransfer(dobj[1], uv=1, ao=dobj[0])
 
-    def createFollicleOnface_UI(self):
+    def createFollicleOnsurface_UI(self):
         ui = 'ToolsBoxUI3'
         try:
             cmds.deleteUI(ui)
@@ -241,46 +283,54 @@ class MaYaToolsBox():
             pass
         cmds.window(ui, t='createFollicleOnface')
         cmds.columnLayout(cat=("both", 2), columnWidth=180, rowSpacing=3)
-        cmds.textFieldGrp('UI3nameTextFieldGrp', l='名称', h=28, cw2=(50, 100))
-        cmds.intFieldGrp('UI3numIntFieldGrp', l='数量', h=28, cw2=(50, 100))
+        cmds.textFieldGrp('UI3nameTextFieldGrp', l=u'名称', h=28, cw2=(50, 100))
+        cmds.intFieldGrp('UI3numIntFieldGrp', l=u'数量', h=28, cw2=(50, 100))
         cmds.flowLayout(columnSpacing=5)
-        cmds.checkBox('UI3JointcheckBox', l='创建骨骼', w=80)
-        cmds.button('UI3RunButton', l="Run", h=28, w=80, c=lambda *args: self.createFollicleOnface(
+        cmds.checkBox('UI3JointcheckBox', l=u'创建骨骼', w=80)
+        cmds.button('UI3RunButton', l="Run", h=28, w=80, c=lambda *args: self.createFollicleOnsurface(
                     cmds.textFieldGrp('UI3nameTextFieldGrp', q=1, tx=1),
                     cmds.intFieldGrp('UI3numIntFieldGrp', q=1, v1=1),
                     cmds.checkBox('UI3JointcheckBox', q=1, v=1)))
         cmds.setParent('..')
         cmds.showWindow(ui)
 
-    def createFollicleOnface(self, name = '', num = '', joint = 0):
+    def createFollicleOnsurface(self, name='', num='', joint=0):
         #UI
-        shape = cmds.listRelatives(cmds.ls(sl=1)[0],s=1,type='nurbsSurface')
+        shape = cmds.listRelatives(cmds.ls(sl=1)[0], s=1, type='nurbsSurface')
         if not shape:
-            cmds.error()
+            om.MGlobal.displayError(u'没选择曲面')
+            return
         Follicle_Grp = name + "_foll_grp"
         Joint_Grp = name + "_Joint_grp"
-        if cmds.ls(Follicle_Grp,typ='transform') or cmds.ls(Joint_Grp,typ='transform'):
-            cmds.error()
+        if cmds.ls(Follicle_Grp, typ='transform') or cmds.ls(Joint_Grp, typ='transform'):
+            om.MGlobal.displayError(u'有重名毛囊或骨骼')
+            return
         cmds.group(em=1, n=Follicle_Grp)
         if joint:
             cmds.group(em=1, n=Joint_Grp)
+        
+        follList = []
         for i in range(num):
             if i == num:
                 break
-            follicT = cmds.rename(cmds.listRelatives(cmds.createNode('follicle'),p=1), name+'_foll')
-            follicS = cmds.rename(cmds.listRelatives(follicT,s=1),name+'_follShape')
-            cmds.connectAttr(shape+".worldSpace[0]", follicS+".inputSurface", f=1)
-            cmds.connectAttr(shape+".worldMatrix[0]", follicS+".inputWorldMatrix", f=1)
-            cmds.connectAttr(follicS+".outTranslate", follicT+".translate", f=1)
-            cmds.connectAttr(follicS+".outRotate", follicT+".rotate", f=1)
-            cmds.setAttr(follicS+".parameterV", .5)
-            cmds.setAttr(follicS+".parameterU", i*1.0/(num-1))
-            cmds.parent(follicT,Follicle_Grp)
-            if cmds.checkBox('UI3JointcheckBox',q=1,v=1):
+            follicS = cmds.createNode('follicle', n='%s_follShape' %name)
+            follicT = cmds.listRelatives(follicS, p=1)[0]
+            
+            cmds.connectAttr("%s.local" %shape[0], "%s.inputSurface" %follicS, f=1)
+            cmds.connectAttr("%s.worldMatrix[0]" %shape[0], "%s.inputWorldMatrix" %follicS, f=1)
+            cmds.connectAttr("%s.outTranslate" %follicS, "%s.translate" %follicT, f=1)
+            cmds.connectAttr("%s.outRotate" %follicS, "%s.rotate" %follicT, f=1)
+            cmds.setAttr("%s.parameterV" %follicS, .5)
+            cmds.setAttr("%s.parameterU" %follicS, i*1.0/(num-1))
+            #cmds.parent(follicT, Follicle_Grp)
+            if joint:
                 cmds.select(cl=1)
-                jointN = cmds.joint(n=name+i+'Joint')
-                cmds.parentConstraint(follicT,jointN,weight=1)
-                cmds.parent(jointN,Joint_Grp)
-    
+                jointN = cmds.joint(n='%s%sJoint' %(name, i))
+                cmds.parentConstraint(follicT, jointN, weight=1)
+                #cmds.parent(jointN, Joint_Grp)
+            follList.append([follicT, jointN])
+        for i in follList:
+            cmds.parent(i[0], Follicle_Grp)
+            cmds.parent(i[1], Joint_Grp)
 
-MaYaToolsBox().ToolUi()
+MayaToolsBox_BlackC().ToolUi()
